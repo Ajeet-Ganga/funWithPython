812f772e70abf5f43a76ac0940320476848ce3ef
Ajeet Ganga <aganga@ebay.com>
2016-03-27 15:24:26 -0700

    Added getFields(fileName):List<fieldType0,fieldType1,fieldName>

--- util.py
+++ util.py
@@ -18,6 +18,28 @@ def getFiles(dirName, fileEnd):
                 fileNameList.append(fileName)
     return fileNameList
 
+# return List<fieldType0,fieldType1,fieldName>
+def getFields(fileName):
+    fieldList = [];
+    p = plyj.parser.Parser()
+    tree = p.parse_file(fileName)
+    for type_decl in tree.type_declarations:
+        for field_decl in [decl for decl in type_decl.body if type(decl) is m.FieldDeclaration]:
+            print(field_decl)
+            for var_decl in field_decl.variable_declarators:
+                type_name0 = ""
+                type_name1 = ""
+                if type(field_decl.type) is str:
+                    type_name0 = field_decl.type
+                else:
+                    type_name0 = field_decl.type.name.value
+                    if len(field_decl.type.type_arguments) > 0:
+                        type_name1 = field_decl.type.type_arguments[0].name.value
+                print(var_decl) 
+                
+                print('    ' + type_name0 + ' ' + type_name1 + ' ' + var_decl.variable.name)
+                fieldList.append((type_name0,type_name1,var_decl.variable.name))
+    return fieldList;
 def todo1:
 	N = collections.namedtuple('N','v,children')
 
