d7348eddca8c221d1eb0598d591713521d502652
Ajeet Ganga <aganga@ebay.com>
2016-03-27 15:44:27 -0700

    Removing print statements.

--- util.py
+++ util.py
@@ -11,15 +11,12 @@ import os
 ################################################################################
 ##### Functions
 ################################################################################
-def getFiles(dirName, fileEnd):
-    print(dirName)
+def getFiles(dirName, fileEnd): 
     fileNameList = []
     for root, dirs, files in os.walk(dirName):
-        print(root, dirs, files);
         for file in files:
             if file.endswith(fileEnd):
                 fileName = os.path.join(root, file)
-                print(fileName)
                 fileNameList.append(fileName)
     return fileNameList
 
@@ -30,7 +27,6 @@ def getFields(fileName):
     tree = p.parse_file(fileName)
     for type_decl in tree.type_declarations:
         for field_decl in [decl for decl in type_decl.body if type(decl) is m.FieldDeclaration]:
-            print(field_decl)
             for var_decl in field_decl.variable_declarators:
                 type_name0 = ""
                 type_name1 = ""
@@ -39,10 +35,7 @@ def getFields(fileName):
                 else:
                     type_name0 = field_decl.type.name.value
                     if len(field_decl.type.type_arguments) > 0:
-                        type_name1 = field_decl.type.type_arguments[0].name.value
-                print(var_decl) 
-                
-                print('    ' + type_name0 + ' ' + type_name1 + ' ' + var_decl.variable.name)
+                        type_name1 = field_decl.type.type_arguments[0].name.value 
                 fieldList.append((type_name0,type_name1,var_decl.variable.name))
     return fieldList;
 def todo1():
