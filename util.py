9fe5a3455650eef2139bc98e640cf3265c5186a2
Ajeet Ganga <aganga@ebay.com>
2016-03-27 14:17:06 -0700

    Added getFiles(dirName, fileEnd)

--- util.py
+++ util.py
@@ -1,12 +1,17 @@
 import collections
 import string
 import sys
-N = collections.namedtuple('N','v,children')
 
-for (c1,c2) in zip(string.lowercase,string.uppercase):
-	setattr(sys.modules[__name__], "n"+c1,N(c2))
 
-na[children]  = (nb,nc);
+
+
+def todo1:
+	N = collections.namedtuple('N','v,children')
+
+	for (c1,c2) in zip(string.lowercase,string.uppercase):
+		setattr(sys.modules[__name__], "n"+c1,N(c2))
+
+	na[children]  = (nb,nc);
 
 	
 
