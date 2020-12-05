3300abd2e9d6258395c13f04cb62a74c04df71d2
ajeet <toajeet@yahoo.com>
2020-12-05 17:02:46 +0530

    Update findpy.py

--- findpy.py
+++ findpy.py
@@ -4,13 +4,14 @@ import datetime
 
 root_dir = '.'
 
-# This prints all the file in root_dir in the format of  "YYYY/MM/DD | FILENAME"
+# This prints all the file in root_dir in the format of  "YYYY/MM/DD | DIRECTORY | FILENAME"
 for directory, subdirectories, files in os.walk(root_dir):
     for file in files:
     	fname = os.path.join(directory, file)
         if (pathlib.Path(fname)):
         	ts_epoch = os.path.getmtime(fname)
         	ts = datetime.datetime.fromtimestamp(ts_epoch).strftime('%Y/%m/%d')
-        	print ts + " | " + fname 
+        	# print ts + " | " + fname 
+        	print ",".join([ts,directory, file])
         else:
         	print 'ERROR: File doesnt exist. file name =' + fname
